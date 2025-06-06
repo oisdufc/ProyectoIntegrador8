6f71f2974d9a50f0044a4f3eb533690f634bb2ab
Pedronadi <pnadinic@udesa.edu.ar>
2025-06-06 09:37:54 -0300

    Puse el genero de las series y peliculas

--- js/detalleSerie.js
+++ js/detalleSerie.js
@@ -14,15 +14,15 @@ fetch(`https://api.themoviedb.org/3/tv/${seriesId}?api_key=4538691d5c60f1ca0445b
 })
 .then(function(data) {
     console.log(data);
-    let series2 = document.querySelector(".series2");
-    series2.innerHTML = `
+    let detalle2 = document.querySelector(".series2");
+    detalle2.innerHTML = `
         <div class="detalle-item">
             <h2>${data.name}</h2>
             <img src="https://image.tmdb.org/t/p/w500${data.poster_path}" alt="${data.name}">
             <p>Fecha de lanzamiento: ${data.first_air_date}</p>
             <p> Descripción: ${data.overview}</p>
             <p>Calificación: ${data.vote_average}</p>
-            <p><a class="link" href="./generosSerie.html">Géneros: ${data.genres.name}</a></p>
+            <p><a class="link" href="./generosSerie.html">Géneros: ${data.genres[0].name}</a></p>
         </div>
     `;
 })
