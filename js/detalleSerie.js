aab8f0d3e08a1cb6a925a0b6117026df444a8946
Pedronadi <pnadinic@udesa.edu.ar>
2025-06-06 16:24:33 -0300

    Arregle algo en el detalleSerie.js

--- js/detalleSerie.js
+++ js/detalleSerie.js
@@ -5,6 +5,8 @@ let queryString = location.search;
 let queryStringObj = new URLSearchParams(queryString);
 
 let seriesId = queryStringObj.get("id");
+let nombregenero1 = queryStringObj.get("nombre");
+console.log(nombregenero1);
 
 console.log(seriesId);
 
@@ -22,7 +24,7 @@ fetch(`https://api.themoviedb.org/3/tv/${seriesId}?api_key=4538691d5c60f1ca0445b
             <p>Fecha de lanzamiento: ${data.first_air_date}</p>
             <p> Descripción: ${data.overview}</p>
             <p>Calificación: ${data.vote_average}</p>
-            <p><a class="link" href="./detalleGeneroSerie.html">Géneros: ${data.genres[0].name}</a></p>
+            <p><a class="link" href="./detalleGeneroSerie.html?id=${data.genres[0].id}">Géneros: ${data.genres[0].name}</a></p>
         </div>
     `;
 })
