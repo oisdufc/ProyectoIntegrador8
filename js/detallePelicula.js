a6560b441c94c7ec8750c571e85317048fdb4f28
Pedronadi <pnadinic@udesa.edu.ar>
2025-06-06 17:47:54 -0300

    .

--- js/detallePelicula.js
+++ js/detallePelicula.js
@@ -1,20 +1,17 @@
 console.log(location.search);
 
 let queryString = location.search;
-
 let queryStringObj = new URLSearchParams(queryString);
 
 let peliculaId = queryStringObj.get("id");
-let nombre2 = queryStringObj.get("nombre2");
-console.log(peliculaId);
 
 fetch(`https://api.themoviedb.org/3/movie/${peliculaId}?api_key=4538691d5c60f1ca0445b38ca446d641&language=es-ES`)
 .then(function(res) {
     return res.json();
 })
 .then(function(data) {
-    console.log(data);
     let detalle = document.querySelector(".detalle");
+    let genero = data.genres[0];
     detalle.innerHTML = `
         <div class="detalle-item">
             <h2>${data.title}</h2>
@@ -23,7 +20,11 @@ fetch(`https://api.themoviedb.org/3/movie/${peliculaId}?api_key=4538691d5c60f1ca
             <p class='parrafo'>Descripción: ${data.overview}</p>
             <p class='parrafo'>Calificación: ${data.vote_average}</p>
             <p class='parrafo'>Duración: ${data.runtime} minutos</p>
-            <p class='parrafo'><a class="link" href="./detalleGeneroPeli.html?id=${data.genres[0].id}&nombre2=${data.genres[0].name}"> Generos: ${data.genres[0].name}</a></p>
+            <p class='parrafo'>
+                <a class="link" href="./detalleGeneroPeli.html?id=${genero.id}&nombre2=${genero.name}">
+                    Géneros: ${genero.name}
+                </a>
+            </p>
         </div>
     `;
 })
